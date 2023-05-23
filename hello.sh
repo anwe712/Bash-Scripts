e4cab6364c30c3aabf0675fb765dc9c1164bd49a
anwe712 <ghoshanwesha272@gmail.com>
2023-05-23 09:18:58 +0530

    shell code added

--- hello.sh
+++ hello.sh
@@ -1,2 +1,12 @@
 #! /usr/bin/bash
 echo "hello world"
+#this is a comment and this is not executed
+echo The bash is $BASH  #gives the name of the bash and is system variable
+echo Our shell version is $BASH_VERSION #gives the version of the bash in use
+echo The home directory is $HOME #gives the home directory 
+echo The current directory is $PWD #gives the current directory
+#user defined variables-
+
+
+name="Anwesha"
+echo "My name is $name "
