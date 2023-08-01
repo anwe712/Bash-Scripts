7daa65c0894541e5c3e8cf8fc0b528f25b0c8a3a
anwe712 <ghoshanwesha272@gmail.com>
2023-08-01 11:59:52 +0530

    odd numbers

---
+++ odd_numbers.sh
@@ -0,0 +1,8 @@
+#only odd numbers are printed here
+echo "Showing all the odd numbers between 1 - 100 "
+for ((i = 1; i <= 99; i+=2))
+do
+    # Commands to be executed for each iteration
+ 
+    echo $i
+done
